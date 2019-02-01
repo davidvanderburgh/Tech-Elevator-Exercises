64ed3a14ff325b0791d20bc0b3603a1c4512fab6
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-02-01 13:59:33 -0500

    Add test method RomanNumeral_Ones

--- module-1/16_Test_Driven_Development/Exercises.Tests/KataRomanNumeralsTests.cs
+++ module-1/16_Test_Driven_Development/Exercises.Tests/KataRomanNumeralsTests.cs
@@ -6,6 +6,20 @@ namespace Exercises.Tests
     [TestClass]
     public class KataRomanNumeralsTests
     {
-        
+        KataRomanNumerals kata;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            kata = new KataRomanNumerals();
+        }
+
+        [TestMethod]
+        public void RomanNumeral_Ones()
+        {
+            Assert.AreEqual("I", kata.ConvertToRomanNumeral(1));
+            Assert.AreEqual("II", kata.ConvertToRomanNumeral(2));
+            Assert.AreEqual("III", kata.ConvertToRomanNumeral(3));
+        }
     }
 }
