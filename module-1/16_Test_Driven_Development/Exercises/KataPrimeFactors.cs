da470cb47543ff8cf3fceae88305318150035473
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-02-01 13:13:37 -0500

    Add passing code for test method Factorize_PrimeNumber_SingleNumber

--- module-1/16_Test_Driven_Development/Exercises/KataPrimeFactors.cs
+++ module-1/16_Test_Driven_Development/Exercises/KataPrimeFactors.cs
@@ -11,7 +11,26 @@ namespace Exercises
 
         public List<int> Factorize(int n)
         {
-            return new List<int>();
+            List<int> result = new List<int>();
+
+            if (n > 1)
+            {
+                for (int i = n; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result.Add(i);
+                    }
+                }
+
+                if (result.Count == 0)
+                {
+                    result.Add(n);
+                }
+
+            }
+
+            return result;
         }
 
     }
