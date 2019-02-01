61eae91f31353cd8761df4a4c906a4d480eb9a13
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-02-01 13:16:45 -0500

    Add test method Factorize_PrimeNumber_MultipleFactors

--- module-1/16_Test_Driven_Development/Exercises.Tests/KataPrimeFactorsTests.cs
+++ module-1/16_Test_Driven_Development/Exercises.Tests/KataPrimeFactorsTests.cs
@@ -23,6 +23,15 @@ namespace Exercises.Tests
             CollectionAssert.AreEqual(new List<int>() { 7 }, kataPrimeFactors.Factorize(7));
         }
 
+        [TestMethod]
+        public void Factorize_PrimeNumber_MultipleFactors()
+        {
+            CollectionAssert.AreEqual(new List<int>() { 2, 2 }, kataPrimeFactors.Factorize(4));
+            CollectionAssert.AreEqual(new List<int>() { 2, 3 }, kataPrimeFactors.Factorize(6));
+            CollectionAssert.AreEqual(new List<int>() { 2, 2, 2 }, kataPrimeFactors.Factorize(8));
+            CollectionAssert.AreEqual(new List<int>() { 3, 3 }, kataPrimeFactors.Factorize(9));
+            CollectionAssert.AreEqual(new List<int>() { 2, 5}, kataPrimeFactors.Factorize(10));
+        }
 
 
 
