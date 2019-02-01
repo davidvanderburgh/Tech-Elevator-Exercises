55d4250e8362ca12aee878ef2dc9d1054d7d3d20
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-02-01 12:18:45 -0500

    Add passing code for test method FizzBuzz_Inside0To100NotDivisibleBy3Or5_NumberAsString

--- module-1/16_Test_Driven_Development/Exercises/KataFizzBuzz.cs
+++ module-1/16_Test_Driven_Development/Exercises/KataFizzBuzz.cs
@@ -10,17 +10,24 @@ namespace Exercises
     {
         public string FizzBuzz(int number)
         {
-            if (number % 3 == 0 && number % 5 == 0)
+            if (number >= 1 && number <= 100)
             {
-                return "FizzBuzz";
-            }
-            else if (number % 3 == 0)
-            {
-                return "Fizz";
-            }
-            else if (number % 5 == 0)
-            {
-                return "Buzz";
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+                else if (number % 3 == 0)
+                {
+                    return "Fizz";
+                }
+                else if (number % 5 == 0)
+                {
+                    return "Buzz";
+                }
+                else
+                {
+                    return number.ToString();
+                }
             }
             else
             {
