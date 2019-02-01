1c2aee90522d2814ba43d334f6fdf8b78bab189d
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-02-01 13:43:30 -0500

    Add passing code for test method Factorize_PrimeNumber_MultipleFactors

--- module-1/16_Test_Driven_Development/Exercises/KataPrimeFactors.cs
+++ module-1/16_Test_Driven_Development/Exercises/KataPrimeFactors.cs
@@ -13,25 +13,31 @@ namespace Exercises
         {
             List<int> result = new List<int>();
 
-            if (n > 1)
+            while(n % 2 == 0)
             {
-                for (int i = n; i < n; i++)
-                {
-                    if (n % i == 0)
-                    {
-                        result.Add(i);
-                    }
-                }
-
-                if (result.Count == 0)
-                {
-                    result.Add(n);
-                }
-
+                result.Add(2);
+                n /= 2;
             }
 
+            int factor = 3;
+            while (factor * factor <= n)
+            {
+                if (n % factor == 0)
+                {
+                    result.Add(factor);
+                    n /= factor;
+                }
+                else
+                {
+                    factor += 2;
+                }
+            }
+
+            if (n > 1)
+            {
+                result.Add(n);
+            }
             return result;
         }
-
     }
 }
