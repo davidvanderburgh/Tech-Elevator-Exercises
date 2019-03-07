3f11a3322a8a07549ad6ed847acc4837352c2c07
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-03-07 11:17:37 -0500

    working on styling

--- module-3/07-Forms-and-Controllers-HTTP-POST/Post.Web/Models/Review.cs
+++ module-3/07-Forms-and-Controllers-HTTP-POST/Post.Web/Models/Review.cs
@@ -8,11 +8,31 @@ namespace Post.Web.Models
 {
     public class Review
     {
+        private int _rating;
+
         [Display(Name="User Name")]
         public string Username { get; set; }
 
         [Display(Name = "Rating")]
-        public int Rating { get; set; }
+        public int Rating
+        {
+            get
+            {
+                return _rating;
+            }
+            set
+            {
+                if (value > 5)
+                {
+                    value = 5;
+                }
+                else if (value < 1)
+                {
+                    value = 1;
+                }
+                _rating = value;
+            }
+        }
 
         [Display(Name = "Review Title")]
         public string ReviewTitle { get; set; }
