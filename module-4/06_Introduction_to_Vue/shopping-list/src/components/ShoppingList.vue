8c646c7116270a798cf7c98f08ce8379f87f83ce
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-03-25 12:27:17 -0400

    finished exercise

---
+++ module-4/06_Introduction_to_Vue/shopping-list/src/components/ShoppingList.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="item-list">
+        <h1>My Shopping List</h1>
+        <ul>
+            <li class="item-completed">{{items[0]}}</li>
+            <li class="item-completed">{{items[1]}}</li>
+            <li class="item-completed">{{items[2]}}</li>
+            <li>{{items[3]}}</li>
+            <li>{{items[4]}}</li>
+            <li>{{items[5]}}</li>
+            <li>{{items[6]}}</li>
+            <li>{{items[7]}}</li>
+            <li>{{items[8]}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        items: [
+          'Oatmeal',
+          'Milk',
+          'Banana',
+          'Strawberries',
+          'Lunch Meat',
+          'Bread',
+          'Grapes',
+          'Steak',
+          'Salad'
+        ]
+    }
+  }
+}
+</script>
+
+<style>
+.item-list {
+    width:450px;
+    background: #fff;
+    margin: 50px auto;
+    font-family: 'Roboto Condensed', sans-serif;
+    border-radius: 10px;
+}
+h1 {
+    background:#f2f2f2;
+    color:#4b86A6;
+    padding:10px;
+    font-size:24px;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 0px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+}
+ul {
+    list-style-type: none;
+    margin:0px;
+    padding:0px;
+}
+li {
+    font-size: 24px;
+    border-bottom:1px solid #f2f2f2;
+    padding:10px 20px;
+}
+li:nth-child(odd) {
+    /* background-color: rgb(129, 182, 129); */
+}
+li:last-child{
+    border:0px;
+}
+li.item-completed {
+    text-decoration: line-through;
+    color: darkgray;
+}
+i.far.fa-check-circle { 
+    float:right;
+    color: darkgray;
+}
+
+i.far.fa-check-circle.completed {
+    color:green;
+}
+</style>
