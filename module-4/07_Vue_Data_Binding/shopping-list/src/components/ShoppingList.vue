a215ca7248aee1779a495dfbf746b7eb707d2392
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-03-26 12:59:54 -0400

    finalized shopping list exercise

--- module-4/07_Vue_Data_Binding/shopping-list/src/components/ShoppingList.vue
+++ module-4/07_Vue_Data_Binding/shopping-list/src/components/ShoppingList.vue
@@ -1,60 +1,97 @@
 <template>
-    <div class="shopping-list">
-        <h1>My Shopping List</h1>
-        <ul>
-            <li class="completed">Oatmeal</li>
-            <li class="completed">Milk</li>
-            <li class="completed">Banana</li>
-            <li>Strawberries</li>
-            <li>Lunch Meat</li>
-            <li>Bread</li>
-            <li>Grapes</li>
-            <li>Steak</li>
-            <li>Salad</li>
-        </ul>
-    </div>
+  <div class="shopping-list">
+    <h1>My Shopping List</h1>
+    <ul>
+        <li 
+        v-for="item in groceries"
+        v-bind:key="item" 
+        :class="{completed:item.completed}"
+        > {{ item.item }} </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      groceries: [
+          {
+              item: "Oatmeal",
+              completed: false
+          }, 
+          {
+              item: "Milk",
+              completed: true
+          }, 
+          {
+              item: "Banana",
+              completed: false
+          }, 
+          {
+              item: "Strawberries",
+              completed: true
+          }, 
+          {
+              item: "Lunch Meat",
+              completed: true
+          }, 
+          {
+              item: "Bread",
+              completed: true
+          }, 
+          {
+              item: "Grapes",
+              completed: false
+          }, 
+          {
+              item: "Steak",
+              completed: true
+          }, 
+          {
+              item: "Salad",
+              completed: false
+          }
+          ]
+    };
+  }
+};
 </script>
 
 <style>
 .shopping-list {
-    width:450px;
-    background: #fff;
-    margin: 50px auto;
-    font-family: 'Roboto Condensed', sans-serif;
-    border-radius: 10px;
+  width: 450px;
+  background: #fff;
+  margin: 50px auto;
+  font-family: "Roboto Condensed", sans-serif;
+  border-radius: 10px;
 }
 h1 {
-    background:#f2f2f2;
-    color:#5c8578;
-    padding:10px;
-    font-size:24px;
-    text-transform: uppercase;
-    text-align: center;
-    margin-bottom: 0px;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
+  background: #f2f2f2;
+  color: #5c8578;
+  padding: 10px;
+  font-size: 24px;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 0px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
 }
 ul {
-    list-style-type: none;
-    margin:0px;
-    padding:0px;
+  list-style-type: none;
+  margin: 0px;
+  padding: 0px;
 }
 li {
-    font-size: 24px;
-    border-bottom:1px solid #f2f2f2;
-    padding:10px 20px;
+  font-size: 24px;
+  border-bottom: 1px solid #f2f2f2;
+  padding: 10px 20px;
 }
-li:last-child{
-    border:0px;
+li:last-child {
+  border: 0px;
 }
 li.completed {
-    text-decoration: line-through;
-    color:darkgrey;
+  text-decoration: line-through;
+  color: darkgrey;
 }
 </style>
