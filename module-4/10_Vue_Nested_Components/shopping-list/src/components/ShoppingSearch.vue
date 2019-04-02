cdcd2f25eb4d9355efe6efa390e670df599fd59e
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-04-02 11:48:27 -0400

    finished exercise

---
+++ module-4/10_Vue_Nested_Components/shopping-list/src/components/ShoppingSearch.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search">
+    <input
+      type="text"
+      id="search"
+      name="search"
+      v-on:keyup="filterItems"
+      placeholder="Search Items..."
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    filterItems() {
+      const query = document.getElementById("search").value;
+      this.$emit("filter-items", query);
+    }
+  }
+};
+</script>
+
+<style>
+#search {
+  width: 100%;
+  font-size: 24px;
+  font-family: "Roboto Condensed", sans-serif;
+  padding: 10px;
+}
+</style>
